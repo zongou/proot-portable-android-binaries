ed032b4447016cc33c35702ffb0a1baa6d34e2b9
zongou <zongou@outlook.com>
2022-04-24 06:02:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ TERMUX_PKG_VERSION=5.1.107
 TERMUX_PKG_REVISION=53
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
 TERMUX_PKG_SHA256=c1572560806657f20639d6e3de4a156bed3fae49fa8f232aaa370846bf324f72
-TERMUX_PKG_DEPENDS="libtalloc-static
+TERMUX_PKG_DEPENDS="libtalloc"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
 
@@ -17,17 +17,14 @@ TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DARG_MAX=131072"
-  LDFLAGS+=" -static"
 }
 
 termux_step_post_make_install() {
-	cd $TERMUX_PKG_SRCDIR/src
+	mkdir -p $TERMUX_PREFIX/share/man/man1
+	install -m600 $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
 
-  sed -i 's/P_tmpdir/"\/tmp"/g' path/temp.c
-
-	make V=1
-	make install
-
-  $STRIP proot
-	cp proot /home/builder/termux-packages
+	sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
+		$TERMUX_PKG_BUILDER_DIR/termux-chroot \
+		> $TERMUX_PREFIX/bin/termux-chroot
+	chmod 700 $TERMUX_PREFIX/bin/termux-chroot
 }
