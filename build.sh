c88f7e787b01191e19cdabc4f8f5f7b2702d5769
zongou <zongou@outlook.com>
2023-02-24 15:24:18 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@ TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 # Just bump commit and version when needed:
-_COMMIT=d4e4a07fbdc16e24868c295d3f7de97eca2a7f80
+_COMMIT=d4658fcfc30d9dfddbfb66e95865fd84bf684abe
 TERMUX_PKG_VERSION=5.1.107
-TERMUX_PKG_REVISION=53
+TERMUX_PKG_REVISION=57
 TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=c1572560806657f20639d6e3de4a156bed3fae49fa8f232aaa370846bf324f72
+TERMUX_PKG_SHA256=49b88f84d7efc2825b352fea2b5c4ab23831aa8f17b65959b0b4c46a263b5111
 TERMUX_PKG_DEPENDS="libtalloc-static"
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
@@ -17,7 +17,7 @@ TERMUX_PKG_EXTRA_MAKE_ARGS="-C src"
 
 termux_step_pre_configure() {
 	CPPFLAGS+=" -DARG_MAX=131072"
-    LDFLAGS+=" -static"
+	LDFLAGS+=" -static"
 }
 
 termux_step_make_install() {
@@ -39,11 +39,11 @@ termux_step_make_install() {
 }
 
 termux_step_post_make_install() {
-    mkdir -p $TERMUX_PREFIX/share/man/man1
-    install -m600 $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
+	mkdir -p $TERMUX_PREFIX/share/man/man1
+	install -m600 $TERMUX_PKG_SRCDIR/doc/proot/man.1 $TERMUX_PREFIX/share/man/man1/proot.1
 
 	# sed -e "s|@TERMUX_PREFIX@|$TERMUX_PREFIX|g" \
-	#     $TERMUX_PKG_BUILDER_DIR/termux-chroot \
-	#     > $TERMUX_PREFIX/bin/termux-chroot
+	# 	$TERMUX_PKG_BUILDER_DIR/termux-chroot \
+	# 	> $TERMUX_PREFIX/bin/termux-chroot
 	# chmod 700 $TERMUX_PREFIX/bin/termux-chroot
 }
